e2e338d18edcf843b81cdb73e7e5e59398714afc
Sameer <achutakumargouda@gmail.com>
2026-06-18 15:25:02 +0530

    Add STATIC_ROOT for Render deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
