6968e20d96f9e2ba75c561f27dfd12fc1ca6843c
Achuta Kumar Gouda <achutakumargouda@gmail.com>
2026-06-18 20:53:03 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,12 +134,15 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR/'static'
 
+ALLOWED_HOSTS = ["https://e-commerce-7dwo.onrender.com"]
+
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:5173',
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
+    'https://e-commerce-1-ussz.onrender.com',
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
