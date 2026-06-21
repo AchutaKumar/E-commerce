829cc679a0773fa22d4ccb38f4b0f524102eb54c
Sameer <achutakumargouda@gmail.com>
2026-06-21 14:35:35 +0530

    Fix SAP refresh 404 issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 import ProductList from "./pages/ProductList";
 import ProductDetail from "./pages/ProductDetails";
 import NavBar from "./components/NavBar";
